183a4d3f457e9dfcdb1291a095ab817547628414
Your Name <victor>
2018-10-22 14:15:54 +0300

    ticket

---
+++ src/com/company/takewill/sate.java
@@ -0,0 +1,13 @@
+package com.company.takewill;
+
+import java.util.Arrays;
+
+public class sate {
+
+    public static void main(String[] args) {
+
+        String[] familie = {"mama", "tata", "eu"};
+        System.out.println(Arrays.toString(familie));
+
+    }
+}

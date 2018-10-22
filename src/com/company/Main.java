ea008461a2f9b56363b50ab4ff92dcf4e23cd444
Elizar <sam@example.com>
2018-10-22 20:25:42 +0300

    Create main file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,22 +1,26 @@
 package com.company;
 
+
+import com.company.takewill.Student;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        StringBuilder sb = new StringBuilder("Ana");
-        StringBuilder sb1 = new StringBuilder("Banana");
-        System.out.println(sb == sb1);
-        StringBuilder sb3 = sb.append("ion");
-        System.out.println(sb3 == sb);
-        if (sb3 == sb){
-            System.out.println(sb3);
+
+            Item item = new Item();
+            System.out.println(item.color);
+            item.setColor("Orange");
+            System.out.println(item.color);
+            item.setColor("Brown");
+       System.out.println(item.color);
+
         }
 
 
-        System.out.println();
+
 
 
 
     }
-}
+

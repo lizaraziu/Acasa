34eada1cc9b2159fb7c17054a8489c730fa02181
Elizar <sam@example.com>
2018-10-26 18:29:19 +0300

    homework 26.10

--- src/com/company/ShapeController.java
+++ src/com/company/ShapeController.java
@@ -1,4 +1,33 @@
 package com.company;
 
 public class ShapeController {
+
+    public static getTrianglePermeter(int lengthOne, int lengthTwo, int lengthTree){
+
+        int tPerimeter = lengthOne + lengthTwo + lengthTree;
+
+        return tPerimeter;
+    }
+
+    public static getRectanglePermeter(int height, int width){
+
+        int rPerimeter = 2*(height + width);
+
+        return rPerimeter;
+
+    }
+
+    static void modifyTriangle(Triangle triangle, int a, int b, int c){
+
+        triangle.lengthOne = a;
+        triangle.lengthTwo = b;
+        triangle.getLengthThree = c;
+    }
+
+    static void modifyTriangle(Rectangle rectangle, int x, int y){
+
+        rectangle.height = x;
+        rectangle.width = y;
+    }
 }
+

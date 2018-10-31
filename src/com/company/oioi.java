16186175df74035937726002e6b524d266ff96c6
Elizar <sam@example.com>
2018-10-31 10:05:25 +0200

    acasa

--- src/com/company/oioi.java
+++ src/com/company/oioi.java
@@ -5,5 +5,6 @@ public class oioi {
     public static void main(String[] args) {
         System.out.println("Hello! ");
         System.out.println("Hello!!! ");
+        System.out.println("hi! ");
     }
 }
